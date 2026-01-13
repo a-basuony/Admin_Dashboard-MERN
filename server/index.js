1086646d5a6f756f5f98f92b2e913a21fb5c2ad0
Ahmed Basuony <A.Basuony.social@gmail.com>
2026-01-13 04:18:10 +0200

    upload

---
+++ server/index.js
@@ -0,0 +1,42 @@
+import express from "express";
+import bodyParser from "body-parser";
+import cors from "cors";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+import helmet from "helmet";
+import morgan from "morgan";
+
+import clientRoutes from "./routes/client.js";
+import generalRoutes from "./routes/general.js";
+import managementRoutes from "./routes/management.js";
+import salesRoutes from "./routes/sales.js";
+
+/* CONFIGURATIONS */
+
+dotenv.config();
+const app = express();
+app.use(express.json());
+app.use(helmet());
+app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" })); // cross-origin sharing request
+app.use(morgan("common"));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cors());
+
+/* ROUTES */
+app.use("/client", clientRoutes); // for pages related to clients like: products, customers, transactions, etc.
+app.use("/general", generalRoutes); // for pages related to general info like: user, dashboard, etc.
+app.use("/management", managementRoutes); // for pages related to management like: overview, admins, performance, etc.
+app.use("/sales", salesRoutes); // for pages related to sales like: overview, daily, monthly, breakdown, etc.
+
+/* MONGOOSE SETUP */
+const PORT = process.env.PORT || 9000;
+mongoose
+  .connect(process.env.MONGO_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    app.listen(PORT, () => console.log(`Server running on port: ${PORT}`));
+  })
+  .catch((error) => console.log(`${error} did not connect`));
